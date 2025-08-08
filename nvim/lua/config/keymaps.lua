132fc9a419fcf9650c4107e7476d51ab890a0a1d
edwinvillota <edwinvillota@hotmail.com>
2025-08-08 14:20:14 -0500

    Added autocompletion and some additional plugins

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,3 +1,15 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+vim.keymap.set("n", "gf", function()
+  return require("obsidian").util.gf_passthrough()
+end, { noremap = false, expr = false, buffer = true })
+
+vim.keymap.set("n", "<leader>ch", function()
+  return require("obsidian").util.toggle_checkbox()
+end, { buffer = true })
+
+vim.keymap.set("n", "<cr>", function()
+  return require("obsidian").util.smart_action()
+end, { expr = true, buffer = true })
---
+++ nvim/lua/plugins/blink-cmp.lua
@@ -0,0 +1,9 @@
+return {
+  "saghen/blink.cmp",
+  opts = {
+    keymap = {
+      preset = "enter", -- keep the default preset
+      ["<M-Space>"] = { "show" }, -- <C-Space> opens the completion menu
+    },
+  },
+}
