7a980493302c642204e65e11fd96756ff2274fa6
edwinvillota <edwinvillota@hotmail.com>
2025-08-18 22:56:18 -0500

    Added jest configuration

--- nvim/lua/plugins/test.lua
+++ nvim/lua/plugins/test.lua
@@ -3,10 +3,12 @@ return {
     "nvim-neotest/neotest",
     dependencies = {
       "marilari88/neotest-vitest",
+      "nvim-neotest/neotest-jest",
     },
     opts = {
       adapters = {
         ["neotest-vitest"] = {},
+        ["neotest-jest"] = {},
       },
     },
   },
